fcee20700f91d631c6254f5d15ab2f7f22ebb42d
xshen <747729633@qq.com>
2023-08-04 20:59:29 +0800

    overload enum define

--- DefEnum/DefEnum.h
+++ DefEnum/DefEnum.h
@@ -8,43 +8,54 @@
 #include <map>
 #include <string>
 
-#define ENUM_CLASS(enumType, valueType, argsDef) \
-    enum class enumType : valueType {            \
-        argsDef(ENUM_VALUE)                      \
-                DEFAULT                          \
-    };                                           \
-    ENUM_NAME(enumType, valueType, argsDef)      \
-    ENUM_TYPE(enumType, valueType, argsDef)
+#define ENUM_CLASS(...) CONCAT(ENUM_CLASS, ARGS_COUNT(__VA_ARGS__))(__VA_ARGS__)
+#define ENUM_CLASS2(enumType, argsDef) ENUM_CLASS3(enumType, int, argsDef)
+#define ENUM_CLASS3(enumType, valueType, argsDef) ENUM_CLASS4(enumType, int, argsDef, DEFAULT)
+#define ENUM_CLASS4(enumType, valueType, argsDef, defaultEnum) \
+    enum class enumType : valueType {                          \
+        argsDef(ENUM_VALUE)                                    \
+                defaultEnum                                    \
+    };                                                         \
+    ENUM_NAME(enumType, valueType, argsDef, defaultEnum)       \
+    ENUM_TYPE(enumType, valueType, argsDef, defaultEnum)
 
-#define ENUM_NAME(enumType, valueType, argsDef)     \
-    const char *StringOf(enumType type) {           \
-        switch (static_cast<valueType>(type)) {     \
-            argsDef(ENUM_CASE) default : return ""; \
-        }                                           \
+#define ENUM_NAME(enumType, valueType, argsDef, defaultEnum)        \
+    const char *StringOf(enumType type) {                           \
+        const char *names[] = {argsDef(ENUM_VALUE_NAME) "DEFAULT"}; \
+        return names[static_cast<int>(type)];                       \
     }
 
-#define ENUM_TYPE(enumType, valueType, argsDef)                                                                           \
-    enumType TypeOf##enumType(const std::string &name) {                                                                  \
-        std::map<std::string, valueType> name2Type = {argsDef(ENUM_PAIR){"", static_cast<valueType>(enumType::DEFAULT)}}; \
-        auto it = name2Type.find(name);                                                                                   \
-        if (it != name2Type.end()) {                                                                                      \
-            return static_cast<enumType>(it->second);                                                                     \
-        }                                                                                                                 \
-        return enumType::DEFAULT;                                                                                         \
+#define ENUM_TYPE(enumType, valueType, argsDef, defaultEnum)                                                                  \
+    enumType TypeOf##enumType(const std::string &name) {                                                                      \
+        valueType id = 0;                                                                                                     \
+        std::map<std::string, valueType> name2Type = {argsDef(ENUM_PAIR){"", static_cast<valueType>(enumType::defaultEnum)}}; \
+        auto it = name2Type.find(name);                                                                                       \
+        if (it != name2Type.end()) {                                                                                          \
+            return static_cast<enumType>(it->second);                                                                         \
+        }                                                                                                                     \
+        return enumType::defaultEnum;                                                                                         \
     }
 
-#define ENUM_VALUE(...) ENUM_VALUE2(__VA_ARGS__)
-#define ENUM_PAIR(...) ENUM_PAIR2(__VA_ARGS__)
-#define ENUM_CASE(...) ENUM_CASE2(__VA_ARGS__)
+#define ENUM_VALUE(...) CONCAT(ENUM_VALUE, ARGS_COUNT(__VA_ARGS__))(__VA_ARGS__)
+#define ENUM_PAIR(...) CONCAT(ENUM_PAIR, ARGS_COUNT(__VA_ARGS__))(__VA_ARGS__)
+#define ENUM_CASE(...) CONCAT(ENUM_CASE, ARGS_COUNT(__VA_ARGS__))(__VA_ARGS__)
+#define ENUM_VALUE_NAME(...) CONCAT(ENUM_VALUE_NAME, ARGS_COUNT(__VA_ARGS__))(__VA_ARGS__)
 
 #define ENUM_VALUE1(value) value,
 #define ENUM_VALUE2(id, value) value = id,
 #define ENUM_VALUE3(id, value, name) value = id,
 
+#define ENUM_PAIR1(value) {#value, id++},
 #define ENUM_PAIR2(id, value) {#value, id},
+#define ENUM_PAIR3(id, value, name) {name, id},
 
-#define ENUM_CASE2(id, value) \
-    case id:                  \
-        return #value;
+#define ENUM_VALUE_NAME1(value) #value,
+#define ENUM_VALUE_NAME2(id, value) #value,
+#define ENUM_VALUE_NAME3(id, value, name) name,
+
+#define CONCAT(a, b) CONCAT_IMPL(a, b)
+#define CONCAT_IMPL(a, b) a##b
+#define ARGS_COUNT(...) ARGS_COUNT_HELPER(__VA_ARGS__, 5, 4, 3, 2, 1)
+#define ARGS_COUNT_HELPER(c5, c4, c3, c2, c1, n, ...) n
 
 #endif//DEFENUM_DEFENUM_H
--- Example/example.cpp
+++ Example/example.cpp
@@ -1,21 +1,45 @@
 //
 // Created by Administrator on 2023/8/4.
 //
-#include <iostream>
 #include "DefEnum/DefEnum.h"
+#include <cstring>
+#include <iostream>
 
 using namespace std;
 
-#define EM_ARGS(x) x(0, a) x(1, b1) x(2, b2)
+#define EM_ARGS1(x) x(a) x(b1) x(b2)
+#define EM_ARGS2(x) x(0, a) x(1, b1) x(2, b2)
+#define EM_ARGS3(x) x(0, a, "A") x(1, b1, "B1") x(2, b2, "B2")
 
-ENUM_CLASS(EMType, int, EM_ARGS)
+ENUM_CLASS(EMType21, EM_ARGS1)
+ENUM_CLASS(EMType22, EM_ARGS2)
+ENUM_CLASS(EMType23, EM_ARGS3)
 
+ENUM_CLASS(EMType1, char, EM_ARGS1)
+ENUM_CLASS(EMType2, char, EM_ARGS2)
+ENUM_CLASS(EMType3, char, EM_ARGS3)
+
+ENUM_CLASS(EMType41, int, EM_ARGS1, COUNT)
+ENUM_CLASS(EMType42, int, EM_ARGS2, COUNT)
+ENUM_CLASS(EMType43, int, EM_ARGS3, COUNT)
+
+#define TEST(type, name)                                             \
+    if (strcmp(StringOf(CONCAT(TypeOf, type)(name)), (name)) != 0) { \
+        cout << "failed: line " << __LINE__ << endl;                 \
+        return 1;                                                    \
+    }
 
 int main() {
-    EMType t = EMType::a;
+    TEST(EMType1, "b1")
+    TEST(EMType2, "b1")
+    TEST(EMType3, "B1")
+    TEST(EMType21, "b1")
+    TEST(EMType22, "b1")
+    TEST(EMType23, "B1")
+    TEST(EMType41, "b1")
+    TEST(EMType42, "b1")
+    TEST(EMType43, "B1")
 
-    cout << StringOf(t) << endl;
-    cout << StringOf(TypeOfEMType("b1")) << endl;
-
+    cout << "success" << endl;
     return 0;
 }
